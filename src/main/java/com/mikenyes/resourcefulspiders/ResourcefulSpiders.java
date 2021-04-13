89275d4c9afe400fe8f3e4b491c99a483b56733b
Dawn Felstar <thelegotechhead@yahoo.com.au>
2021-04-13 11:06:10 +1000

    Initial Setup

---
+++ src/main/java/com/mikenyes/resourcefulspiders/ResourcefulSpiders.java
@@ -0,0 +1,17 @@
+package com.mikenyes.resourcefulspiders;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("resourcefulspiders")
+public class ResourcefulSpiders {
+
+    public static final String MOD_ID = "resourcefulspiders";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public ResourcefulSpiders() {
+
+    }
+}
